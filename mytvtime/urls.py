6aa1d3897d88471d3811325ad2dab204563e74de
Lxnii <27051542+Lxnii@users.noreply.github.com>
2023-07-08 16:03:33 -0700

    Implemented Watching List database caching instead of real-time pulling.

--- mytvtime/urls.py
+++ mytvtime/urls.py
@@ -6,10 +6,10 @@ app_name = 'mytvtime'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search_results, name='search_results'),
-    path('add_to_watchlist/<int:trakt_id>/', views.add_to_watchlist, name='add_to_watchlist'),
+    path('add_to_watchlist/<int:trakt_id>/', views.add_show_to_watchlist, name='add_to_watchlist'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('api/get_shows/', views.get_watching_shows),
-    path('remove_from_watchlist/', views.remove_from_watchlist, name='remove_from_watchlist'),
+    path('get_watching_shows/', views.get_watching_shows),
+    path('remove_from_watchlist/', views.remove_show_from_watchlist, name='remove_from_watchlist'),
 ]
