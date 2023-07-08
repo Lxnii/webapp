b6ade8c6b9ca2aa47a77719310cccc523c05cc9f
Lxnii <27051542+Lxnii@users.noreply.github.com>
2023-07-08 16:03:33 -0700

    Implemented Watching List database caching instead of real-time pulling.

--- mytvtime/admin.py
+++ mytvtime/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Show, Watchlist
+from .models import Show, Watchlist, NextEpisode
 # Register your models here.
 
 admin.site.register(Show)
 admin.site.register(Watchlist)
+admin.site.register(NextEpisode)
