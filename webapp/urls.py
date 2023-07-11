1232cccb53016390e067a19db0f6e91c2958899e
Lxnii <27051542+Lxnii@users.noreply.github.com>
2023-07-10 23:28:53 -0700

    update urls file

--- webapp/urls.py
+++ webapp/urls.py
@@ -20,5 +20,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mytvtime/', include("mytvtime.urls")),
+    path('', include("mytvtime.urls")),
 ]
