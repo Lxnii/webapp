b60339b90bf2ed0a59cf2a80492de04cfafd6bc8
Lxnii <27051542+Lxnii@users.noreply.github.com>
2023-07-14 01:36:26 -0700

    adjust schedule refresh

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from webapp.scheduler import start
+from webapp.scheduler import start_jobs
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
 
 application = get_wsgi_application()
 
-start()
+start_jobs()
