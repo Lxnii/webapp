50c16b036ccfb1b7863329ee7a25771339e4aeb2
Lxnii <27051542+Lxnii@users.noreply.github.com>
2023-07-14 01:36:26 -0700

    adjust schedule refresh

--- webapp/scheduler.py
+++ webapp/scheduler.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
 from mytvtime.jobs import update_shows
 
-def start():
+def start_jobs():
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
     scheduler.add_job(update_shows, 'interval', minutes=1, id='update_all_shows', replace_existing=True)
